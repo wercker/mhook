9442376a7afe341ce6d614500760d9d5668cad19
Matthew Hooker <mwhooker@gmail.com>
2015-10-16 16:40:15 -0700

    add upload command

--- mhook.go
+++ mhook.go
@@ -30,20 +30,21 @@ import (
 // s3://$bucket/$project/$branch/latest/*	<- latest artifacts
 // s3://$bucket/$project/$branch/$commit/*	<- artifacts at commit id
 
-type Fetcher struct {
-	S3          *s3.S3
-	Bucket      string
-	Project     string
-	Branch      string
-	Commit      string
-	Destination string
+type Mhook struct {
+	S3           *s3.S3
+	Bucket       string
+	Project      string
+	Branch       string
+	Commit       string
+	Destination  string
+	ShowProgress bool
 }
 
-func (f *Fetcher) HeadKey() *string {
+func (f *Mhook) HeadKey() *string {
 	return aws.String(fmt.Sprintf("/%s/%s/HEAD", f.Project, f.Branch))
 }
 
-func (f *Fetcher) Key(target string) *string {
+func (f *Mhook) Key(target string) *string {
 	return aws.String(fmt.Sprintf("/%s/%s/%s/%s", f.Project, f.Branch, f.Commit, target))
 }
 
@@ -73,7 +74,7 @@ func targetPathWritable(path string) (bool, error) {
 }
 
 // Head prints the git hash of the latest version
-func Head(opts *Fetcher) string {
+func Head(opts *Mhook) string {
 	resp, err := opts.S3.GetObject(&s3.GetObjectInput{
 		Bucket: aws.String(opts.Bucket),
 		Key:    opts.HeadKey(),
@@ -90,10 +91,10 @@ func Head(opts *Fetcher) string {
 	return string(etag)
 }
 
-func targetSize(opts *Fetcher, target string) *int64 {
-	resp, err := opts.S3.HeadObject(&s3.HeadObjectInput{
-		Bucket: aws.String(opts.Bucket),
-		Key:    opts.Key(target),
+func (m *Mhook) targetSize(target string) *int64 {
+	resp, err := m.S3.HeadObject(&s3.HeadObjectInput{
+		Bucket: aws.String(m.Bucket),
+		Key:    m.Key(target),
 	})
 	if err != nil {
 		panic(err)
@@ -112,32 +113,59 @@ func (pw *ProgressWriter) WriteAt(p []byte, off int64) (int, error) {
 	return pw.w.WriteAt(p, off)
 }
 
+func (m *Mhook) Upload(source string, prefix string) error {
+	uploader := s3manager.NewUploaderWithClient(m.S3)
+	walk := func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		bar := pb.New64(info.Size()).SetUnits(pb.U_BYTES)
+		if m.ShowProgress {
+			bar.Start()
+		}
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		reader := io.TeeReader(file, bar)
+		uploadInput := &s3manager.UploadInput{
+			Bucket: aws.String(m.Bucket),
+			Key:    m.Key(prefix + path),
+			Body:   reader,
+		}
+		fmt.Println(*uploadInput.Key)
+		_, err = uploader.Upload(uploadInput)
+		return err
+	}
+	return filepath.Walk(filepath.Clean(source), walk)
+}
+
 // Fetch fetches target from path specified in opts
-func Fetch(opts *Fetcher, target string, showProgress bool) error {
-	targetPath := filepath.Dir(opts.Destination)
+func (m *Mhook) Fetch(target string, destination string) error {
+	targetPath := filepath.Dir(destination)
 	writable, err := targetPathWritable(targetPath)
 	if !writable || err != nil {
 		fmt.Printf("Cannot write to target `%s`. Please check that it exists and is writable.\n", targetPath)
 		return err
 	}
 
-	temp, err := ioutil.TempFile(targetPath, fmt.Sprintf(".%s-", opts.Project))
+	temp, err := ioutil.TempFile(targetPath, fmt.Sprintf(".%s-", m.Project))
 	if err != nil {
 		return err
 	}
 	defer temp.Close()
 
-	bar := pb.New64(*targetSize(opts, target)).SetUnits(pb.U_BYTES)
-	if showProgress {
+	bar := pb.New64(*m.targetSize(target)).SetUnits(pb.U_BYTES)
+	if m.ShowProgress {
 		bar.Start()
 	}
-	etag := readMD5Sum(opts.Destination)
+	etag := readMD5Sum(destination)
 	writer := &ProgressWriter{temp, bar}
 
-	downloader := s3manager.NewDownloaderWithClient(opts.S3)
+	downloader := s3manager.NewDownloaderWithClient(m.S3)
 	_, err = downloader.Download(writer, &s3.GetObjectInput{
-		Bucket:      aws.String(opts.Bucket),
-		Key:         opts.Key(target),
+		Bucket:      aws.String(m.Bucket),
+		Key:         m.Key(target),
 		IfNoneMatch: aws.String(etag),
 	})
 	if err != nil {
@@ -153,10 +181,10 @@ func Fetch(opts *Fetcher, target string, showProgress bool) error {
 		return err
 	}
 
-	return os.Rename(temp.Name(), opts.Destination)
+	return os.Rename(temp.Name(), destination)
 }
 
-func collectOptions(c *cli.Context) *Fetcher {
+func collectOptions(c *cli.Context) *Mhook {
 
 	if c.String("bucket") == "" {
 		println("Error: bucket cannot be empty.")
@@ -172,12 +200,13 @@ func collectOptions(c *cli.Context) *Fetcher {
 	region := c.String("region")
 	sess := session.New(&aws.Config{Region: &region})
 	svc := s3.New(sess)
-	return &Fetcher{
-		S3:      svc,
-		Bucket:  c.String("bucket"),
-		Project: c.String("project"),
-		Branch:  c.String("branch"),
-		Commit:  c.String("commit"),
+	return &Mhook{
+		S3:           svc,
+		Bucket:       c.String("bucket"),
+		Project:      c.String("project"),
+		Branch:       c.String("branch"),
+		Commit:       c.String("commit"),
+		ShowProgress: termutil.Isatty(os.Stdout.Fd()),
 	}
 }
 
@@ -190,7 +219,7 @@ func globalFlags() []cli.Flag {
 	}
 }
 
-func fetchFlags() []cli.Flag {
+func downloadFlags() []cli.Flag {
 	flags := []cli.Flag{
 		cli.StringFlag{Name: "commit, c", Value: "latest", Usage: "git commit (or 'latest')"},
 	}
@@ -198,44 +227,85 @@ func fetchFlags() []cli.Flag {
 	return flags
 }
 
+func uploadFlags() []cli.Flag {
+	flags := []cli.Flag{
+		cli.StringFlag{Name: "commit, c", Value: "latest", Usage: "git commit (or 'latest')"},
+	}
+	flags = append(flags, globalFlags()...)
+	return flags
+}
+
+var (
+	headCommand = cli.Command{
+		Name:  "head",
+		Usage: "Print latest commit.",
+		Action: func(c *cli.Context) {
+			opts := collectOptions(c)
+			fmt.Print(Head(opts))
+		},
+		Flags: globalFlags(),
+	}
+	downloadCommand = cli.Command{
+		Name:      "download",
+		Usage:     "Download mhook artifact. If no destination is supplied, use the base path of the target.",
+		ArgsUsage: "<target> [destination]",
+		Action: func(c *cli.Context) {
+			// Check for credentials and well-formedness, then call Fetch
+
+			if !c.Args().Present() {
+				cli.ShowAppHelp(c)
+				os.Exit(1)
+			}
+
+			mhook := collectOptions(c)
+			var destination string
+			target := c.Args().First()
+
+			destination = c.Args().Get(1)
+			if destination == "" {
+				// Our destination file will be the same name as our target basename
+				destination = path.Base(target)
+			}
+
+			fmt.Printf("Downloading from %s\n", *mhook.Key(target))
+			if err := mhook.Fetch(target, destination); err != nil {
+				panic(err)
+			}
+		},
+		Flags: downloadFlags(),
+	}
+	uploadCommand = cli.Command{
+		Name:      "upload",
+		Usage:     "Upload mhook artifact.",
+		ArgsUsage: "<source> [upload prefix]",
+		Action: func(c *cli.Context) {
+			if !c.Args().Present() {
+				cli.ShowAppHelp(c)
+				os.Exit(1)
+			}
+			mhook := collectOptions(c)
+			source := c.Args().First()
+			prefix := c.Args().Get(1)
+			// if target is directory, upload it recursively
+			if err := mhook.Upload(source, prefix); err != nil {
+				panic(err)
+			}
+		},
+		Flags: uploadFlags(),
+	}
+)
+
 func main() {
 	app := cli.NewApp()
 	app.Name = "mhook"
-	app.Usage = "[global options] path [dest]"
-	app.Flags = fetchFlags()
+	app.Usage = "Manage the MUFL"
+	// Set downloadCommand as default for backwards compatibility
+	app.Flags = downloadCommand.Flags
 	app.Commands = []cli.Command{
-		{
-			Name:  "head",
-			Usage: "print latest commit",
-			Action: func(c *cli.Context) {
-				opts := collectOptions(c)
-				fmt.Print(Head(opts))
-			},
-			Flags: globalFlags(),
-		},
+		headCommand,
+		downloadCommand,
+		uploadCommand,
 	}
-	app.Action = func(c *cli.Context) {
-		// Check for credentials and well-formedness, then call Fetch
-
-		if len(c.Args()) < 1 {
-			cli.ShowAppHelp(c)
-			os.Exit(1)
-		}
-
-		opts := collectOptions(c)
-		target := c.Args()[0]
-
-		if len(c.Args()) < 2 {
-			// Our destination file will be the same name as our target basename
-			opts.Destination = path.Base(target)
-		} else {
-			opts.Destination = c.Args()[1]
-		}
-
-		fmt.Printf("Downloading from %s\n", *opts.Key(target))
-		if err := Fetch(opts, target, termutil.Isatty(os.Stdout.Fd())); err != nil {
-			panic(err)
-		}
-	}
+	app.Action = downloadCommand.Action
 	app.Run(os.Args)
 }
